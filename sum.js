2a5190b378b9451aa88683662581bd575729c81b
xstartstaticx <daniel.reiller@icloud.com>
2022-07-18 13:37:00 +0200

    finished sum.js

--- sum.js
+++ sum.js
@@ -1,6 +1,11 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
-
-const sum = () => {};
+function sum() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
 
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
